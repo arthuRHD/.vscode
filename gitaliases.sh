1a565c7d3ab3d2297a7effa1c1b74b030f5e30a8
Arthur RICHARD <arthur.richard@protonmail.com>
2020-07-29 19:44:13 +0200

    First commit

---
+++ gitaliases.sh
@@ -0,0 +1,8 @@
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.cm "commit -m"
+git config --global alias.st status
+git config --global alias.render "log --graph --oneline --all --decorate"
+git config --global alias.aa "add ."
+git config --global alias.cp cherry-pick
+git config --global alias.rb rebase
---
+++ install.sh
@@ -0,0 +1,15 @@
+code --install-extension almenon.arepl
+code --install-extension DavidAnson.vscode-markdownlint
+code --install-extension eamodio.gitlens
+code --install-extension donjayamanne.githistory
+code --install-extension esbenp.prettier-vscode
+code --install-extension hbenl.vscode-test-explorer
+code --install-extension janisdd.vscode-edit-csv
+code --install-extension littlefoxteam.vscode-python-test-adapter
+code --install-extension mgesbert.python-path
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-python.python
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-vscode-remote.remote-ssh-edit
+code --install-extension PKief.material-icon-theme
+code --install-extension VisualStudioExptTeam.vscodeintellicode
